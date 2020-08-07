1c9c94c4eee939257fb4a7ac025fc57172addfdc
JERRY JOHN STEPHEN <39152522+jerry619@users.noreply.github.com>
2020-08-07 18:26:58 +0200

    Update init.sh

--- init/init.sh
+++ init/init.sh
@@ -6,7 +6,7 @@ helm init --service-account tiller --tiller-namespace prod --upgrade
 helm init --service-account tiller --tiller-namespace dev --upgrade
 helm init --service-account tiller --tiller-namespace devops --upgrade
 # Adding helm repo and updating
-helm repo add jjjje https://b19c46a684198de21ca4b58f37b287bd7a3a25c3@raw.githubusercontent.com/jerry619/helmcharts/master/
+helm repo add jjjje https://raw.githubusercontent.com/jerry619/helmcharts/master
 helm repo update
 # Generating Kubeconfig specially to be used in Jenkins with restricted access
 sh kubeconfig.sh
